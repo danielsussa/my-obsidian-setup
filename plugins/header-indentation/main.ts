84dfd000a43427ab935703a9dd8cefe8efb7d966
danielsussa <danielsussa@gmail.com>
2022-10-05 19:04:48 -0300

    add indentation

---
+++ plugins/header-indentation/main.ts
@@ -0,0 +1,84 @@
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	ViewPlugin,
+	ViewUpdate,
+} from "@codemirror/view";
+import {RangeSetBuilder} from "@codemirror/state";
+// @ts-ignore
+
+
+
+const indentHeading = ViewPlugin.fromClass(class {
+	decorations: DecorationSet
+
+	constructor(view: EditorView) {
+		this.decorations = indentHeadingExec(view)
+	}
+
+	update(update: ViewUpdate) {
+		this.decorations = indentHeadingExec(update.view)
+	}
+}, {
+	decorations: v => v.decorations,
+
+})
+
+function indentHeadingExec(view: EditorView) {
+
+	const builder = new RangeSetBuilder<Decoration>()
+
+	let currClass = "cm-line-child-p"
+
+	for (const {from, to} of view.visibleRanges) {
+		for (let pos = from; pos <= to;) {
+
+			const line = view.state.doc.lineAt(pos)
+			const lineText = view.state.doc.lineAt(pos)
+			pos = line.to + 1
+
+			if (lineText.text.startsWith("###### ")) {
+				currClass = "cm-line-child-h6"
+				continue
+			}
+			if (lineText.text.startsWith("##### ")) {
+				currClass = "cm-line-child-h5"
+				continue
+			}
+			if (lineText.text.startsWith("#### ")) {
+				currClass = "cm-line-child-h4"
+				continue
+			}
+			if (lineText.text.startsWith("### ")) {
+				currClass = "cm-line-child-h3"
+				continue
+			}
+			if (lineText.text.startsWith("## ")) {
+				currClass = "cm-line-child-h2"
+				continue
+			}
+			if (lineText.text.startsWith("# ")) {
+				currClass = "cm-line-child-h1"
+				continue
+			}
+
+			builder.add(line.from, line.from, Decoration.line({
+				attributes: {class: currClass}
+			}))
+
+		}
+	}
+	return builder.finish()
+}
+
+export default class MyPlugin extends Plugin {
+	async onload() {
+		this.registerEditorExtension(indentHeading)
+	}
+
+	onunload() {
+	}
+
+}
